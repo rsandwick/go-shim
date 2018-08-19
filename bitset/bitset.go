f25f76aea54e2cd204d1fcf41a8e2a921451f163
raynard <rsandwick3@gmail.com>
2018-08-19 18:44:51 -0400

    add public interface for consumers
    - add BitSet interface
    - expose size and storage via interface rather than direct struct accessors

--- bitset/bitset.go
+++ bitset/bitset.go
@@ -3,24 +3,41 @@ package bitset
 var bitsPerInt uint = 64
 
 type bitSet struct {
-	Size    uint
-	Storage []uint64
+	size    uint
+	storage []uint64
+}
+
+type BitSet interface {
+	Clear(uint)
+	GetStorage() *[]uint64
+	MemorySize() uint
+	Set(uint)
+	Size() uint
+	Test(uint) bool
 }
 
 func (bs *bitSet) Clear(i uint) {
-	(*bs).Storage[i/bitsPerInt] &^= 1 << (i % bitsPerInt)
+	(*bs).storage[i/bitsPerInt] &^= 1 << (i % bitsPerInt)
+}
+
+func (bs *bitSet) GetStorage() *[]uint64 {
+	return &((*bs).storage)
 }
 
 func (bs *bitSet) MemorySize() uint {
-	return uint(len((*bs).Storage)) * (bitsPerInt / 8)
+	return uint(len((*bs).storage)) * (bitsPerInt / 8)
 }
 
 func (bs *bitSet) Set(i uint) {
-	(*bs).Storage[i/bitsPerInt] |= 1 << (i % bitsPerInt)
+	(*bs).storage[i/bitsPerInt] |= 1 << (i % bitsPerInt)
+}
+
+func (bs *bitSet) Size() uint {
+	return (*bs).size
 }
 
 func (bs *bitSet) Test(i uint) bool {
-	return (((*bs).Storage[i/bitsPerInt]>>(i%bitsPerInt))&1 == 1)
+	return (((*bs).storage[i/bitsPerInt]>>(i%bitsPerInt))&1 == 1)
 }
 
 func New(size uint) *bitSet {
